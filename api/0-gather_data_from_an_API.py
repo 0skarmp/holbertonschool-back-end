2e5809d2b7981658b2e7136064004ea9e06dbe62
0skarmp <omoralespj@gmail.com>
2023-09-24 21:39:40 -0700

    fix bugs

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""Python script that using REST API"""
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    user_id = sys.argv[1]
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    response1 = requests.get(todo_url).json()
+    response2 = requests.get(user_url).json()
+
+    number_tasks_done = sum(1 for task in response1 if task["completed"])
+    total_tasks = len(response1)
+
+    employee_name = response2.get("name")
+    task_titles = [task["title"] for task in response1 if task["completed"]]
+
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+          employee_name, number_tasks_done, total_tasks))
+    for title in task_titles:
+        print(f"\t {title}")
